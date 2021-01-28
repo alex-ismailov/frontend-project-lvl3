edf7a0d6c1f327320cbb310b9b620d7171a3e1da
smlnx <alexander.is.dev@gmail.com>
2021-01-28 03:03:07 +0300

    Setup(webpack.config.cjs) source mapping

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,5 +1,7 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 const path = require('path');
 
 const PATHS = {
@@ -9,19 +11,54 @@ const PATHS = {
 };
 
 module.exports = {
-  entry: PATHS.src,
+  entry: {
+    main: PATHS.src,
+  },
   output: {
     path: PATHS.dist,
-    filename: 'index_bundle.js'
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      }
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    // TODO: Move to webpack.dev.conf.js !!! ***
+    // Site map is private
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map'
+    }),
+    // ***********************************
     new HtmlWebpackPlugin({
       template: `${PATHS.src}/index.html`,
     }),
   ],
+  
   devServer: {
-    // contentBase: path.join(__dirname, 'dist'),
+    contentBase: PATHS.dist,
     compress: true,
     port: 9000,
     overlay: {
