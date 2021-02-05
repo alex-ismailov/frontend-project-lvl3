7f57e45eb96abca290e0c97462abf512583618f9
smlnx <alexander.is.dev@gmail.com>
2021-02-06 01:58:06 +0300

    Add(src/js/) parser.js

---
+++ src/js/parser.js
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+
+const buildFeed = (feedXmlDocument) => {
+  console.log(feedXmlDocument);
+  const feedId = _.uniqueId();
+  const feedTitle = feedXmlDocument.querySelector('title').textContent;
+  const feedDescription = feedXmlDocument.querySelector('description').textContent;
+  const feedLink = feedXmlDocument.querySelector('link').textContent;
+  const items = feedXmlDocument.querySelectorAll('item');
+  const feedPosts = Array.from(items).map((item) => {
+    const postId = _.uniqueId();
+    const postTitle = item.querySelector('title').textContent;
+    const postDescription = item.querySelector('description').textContent;
+    const postLink = item.querySelector('link').textContent;
+    return {
+      post: postId,
+      feedId,
+      title: postTitle,
+      description: postDescription,
+      link: postLink,
+    };
+  });
+
+  return {
+    feed: {
+      id: feedId,
+      title: feedTitle,
+      description: feedDescription,
+      link: feedLink,
+    },
+    posts: feedPosts,
+  };
+};
+
+export default (data) => {
+  const parser = new DOMParser();
+  const feedXmlDocument = parser.parseFromString(data.data.contents, 'text/xml');
+  const feed = buildFeed(feedXmlDocument);
+
+  return feed;
+};
