f9c8fc56a1b2b5715cafcf9ad3e8508ff319e5a5
smlnx <alexander.is.dev@gmail.com>
2021-02-05 22:37:40 +0300

    Add(src/js/init.js) rss fetching

--- src/js/init.js
+++ src/js/init.js
@@ -1,19 +1,37 @@
+import axios from 'axios';
 import onChange from 'on-change';
 import * as yup from 'yup';
+import _ from 'lodash';
 import { renderInput, renderError } from './view.js';
 
 // *** MVC: MODEL -> VIEW -> CONTROLLER ->> MODEL ......***
 // ********************************************************
 
-const schema = yup.string().required().url();// надо уточнить это пайплайн
+const schema = yup.string().required().url('Must be valid url');// надо уточнить это пайплайн
 
-const validate = (field) => {
+// const validate = (field) => {
+//   console.log(field);
+//   try {
+//     schema.validateSync(field, { abortEarly: false });
+//     return '';
+//   } catch (e) {
+//     const message = e.message.split(' ').slice(1).join(' ');
+//     return message;
+//   }
+// };
+
+const validate = (watchedState) => {
+  const { form: { value }, feeds } = watchedState;
   try {
-    schema.validateSync(field, { abortEarly: false });
+    schema.validateSync(value, { abortEarly: false });
+    if (feeds.some((feed) => feed.link === value)) {
+      console.log('Double');
+      return 'Rss already exists';
+    }
     return '';
   } catch (e) {
-    const message = e.message.split(' ').slice(1).join(' ');
-    return message;
+    console.log(e);
+    return e.message;
   }
 };
 
@@ -26,11 +44,19 @@ export default () => {
   // *** MODEL ***
   const state = {
     form: {
+      processState: 'filling', // sending, finished || failed
       value: '',
       valid: true,
       error: '',
     },
-    news: [],
+    feeds: [
+      // {
+      //   id: 1,
+      //   title: 'Feed 1',
+      //   description: 'Feed 1 description',
+      //   link: 'https://ru.hexlet.io/lessons.rss',
+      // },
+    ],
   };
 
   const form = document.getElementById('rssForm');
@@ -40,15 +66,56 @@ export default () => {
   const watchedState = onChange(state, (path, value) => {
     switch (path) {
       case 'form.valid':
+        console.log(value);
         renderInput(value, input);
         break;
       case 'form.error':
         renderError(value, feedback);
         break;
+      case 'form.processState':
+        break;
       default:
         break;
     }
   });
+
+  // *** utils ***
+  const buildFeed = (feedXmlDocument) => {
+    const feedId = _.uniqueId();
+    const feedTitle = feedXmlDocument.querySelector('title').textContent;
+    const feedDescription = feedXmlDocument.querySelector('description').textContent;
+    const feedLink = feedXmlDocument.querySelector('link').textContent;
+    const items = feedXmlDocument.querySelectorAll('item');
+    const feedPosts = Array.from(items).map((item) => {
+      const postId = _.uniqueId();
+      const postTitle = item.querySelector('title').textContent;
+      const postDescription = item.querySelector('description').textContent;
+      const postLink = item.querySelector('link').textContent;
+      return {
+        post: postId,
+        feedId,
+        title: postTitle,
+        description: postDescription,
+        link: postLink,
+      };
+    });
+
+    return {
+      id: feedId,
+      title: feedTitle,
+      description: feedDescription,
+      link: feedLink,
+      posts: feedPosts,
+    };
+  };
+
+  const parse = (data) => {
+    const parser = new DOMParser();
+    const feedXmlDocument = parser.parseFromString(data.data.contents, 'text/xml');
+    const feed = buildFeed(feedXmlDocument);
+
+    return feed;
+  };
 
   // *** CONTROLLERS ***
   form.addEventListener('input', (e) => {
@@ -58,19 +125,51 @@ export default () => {
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const error = validate(watchedState.form.value);
+    const error = validate(watchedState);
     if (error) {
+      console.log('ERROR not valid url');
       watchedState.form.valid = false;
       watchedState.form.error = error;
       return;
     }
 
-    /* TODO: записать новые данные */
-    // const data = watchedState.form.value;
+    // sending stage
+    /* TODO: получить Фид */
+    const urlWithAllOriginsProxy = `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(watchedState.form.value)}`;
+    axios.get(urlWithAllOriginsProxy).then((response) => {
+      const feed = parse(response);
+      console.log(feed);
+    });
+
     // ********
-    watchedState.form.valid = true;
-    watchedState.form.error = '';
-    watchedState.form.value = '';
+
+    // watchedState.processState = 'sending';
+
+    // watchedState.form.valid = true;
+    // watchedState.form.error = '';
+    // watchedState.form.value = '';
   });
+
+  // form.addEventListener('submit', (e) => {
+  //   e.preventDefault();
+  //   const error = validate(watchedState.form.value);
+  //   console.log(error);
+  //   if (error) {
+  //     console.log('ERROR not valid url');
+  //     watchedState.form.valid = false;
+  //     watchedState.form.error = error;
+  //     return;
+  //   }
+
+  //   /* TODO: записать новые данные */
+  //   // const data = watchedState.form.value;
+  //   // ********
+  //   console.log('valid url :)');
+
+  //   watchedState.form.valid = true;
+  //   watchedState.form.error = '';
+  //   watchedState.form.value = '';
+
+  // });
   // ******
 };
