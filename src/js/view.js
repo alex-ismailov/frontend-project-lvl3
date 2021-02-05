90ffdef2a599b8b0d41e4190aada8051f2307ce5
smlnx <alexander.is.dev@gmail.com>
2021-02-06 01:59:54 +0300

    Remove(src/js/view.js) capitalizeFirstLetter function

--- src/js/view.js
+++ src/js/view.js
@@ -1,14 +1,11 @@
 /* eslint no-param-reassign: 0 */
 
-const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-
 export const renderInput = (isValid, input) => {
   if (!isValid) {
     input.classList.add('is-invalid');
     return;
   }
   input.classList.remove('is-invalid');
-  input.value = '';
 };
 
 export const renderError = (error, feedback) => {
@@ -17,6 +14,6 @@ export const renderError = (error, feedback) => {
     feedback.classList.remove('text-danger');
     return;
   }
-  feedback.textContent = capitalizeFirstLetter(error);
+  feedback.textContent = error;
   feedback.classList.add('text-danger');
 };
