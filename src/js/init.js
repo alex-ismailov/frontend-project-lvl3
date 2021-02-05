e5d30445d1ecd3eaca347c54a96a8436eb5d11a5
smlnx <alexander.is.dev@gmail.com>
2021-02-05 23:19:23 +0300

    Add(src/js/init.js) button disabling on sending process state

--- src/js/init.js
+++ src/js/init.js
@@ -35,7 +35,47 @@ const validate = (watchedState) => {
   }
 };
 
+const buildFeed = (feedXmlDocument) => {
+  const feedId = _.uniqueId();
+  const feedTitle = feedXmlDocument.querySelector('title').textContent;
+  const feedDescription = feedXmlDocument.querySelector('description').textContent;
+  const feedLink = feedXmlDocument.querySelector('link').textContent;
+  const items = feedXmlDocument.querySelectorAll('item');
+  const feedPosts = Array.from(items).map((item) => {
+    const postId = _.uniqueId();
+    const postTitle = item.querySelector('title').textContent;
+    const postDescription = item.querySelector('description').textContent;
+    const postLink = item.querySelector('link').textContent;
+    return {
+      post: postId,
+      feedId,
+      title: postTitle,
+      description: postDescription,
+      link: postLink,
+    };
+  });
+
+  return {
+    feed: {
+      id: feedId,
+      title: feedTitle,
+      description: feedDescription,
+      link: feedLink,
+    },
+    posts: feedPosts,
+  };
+};
+
+const parse = (data) => {
+  const parser = new DOMParser();
+  const feedXmlDocument = parser.parseFromString(data.data.contents, 'text/xml');
+  const feed = buildFeed(feedXmlDocument);
+
+  return feed;
+};
+
 // *** VIEW ***
+// look at src/js/view.js
 // ************
 
 // **********************************************************************
@@ -57,65 +97,43 @@ export default () => {
       //   link: 'https://ru.hexlet.io/lessons.rss',
       // },
     ],
+    posts: [],
   };
 
   const form = document.getElementById('rssForm');
   const input = form.elements.url;
   const feedback = document.querySelector('.feedback');
+  const submitButton = form.querySelector('button');
 
   const watchedState = onChange(state, (path, value) => {
     switch (path) {
+      case 'form.processState': {
+        console.log(`is finished: ${value}`);
+        if (value === 'finished') {
+          input.value = '';
+          break;
+        }
+        break;
+      }
       case 'form.valid':
-        console.log(value);
         renderInput(value, input);
         break;
       case 'form.error':
         renderError(value, feedback);
         break;
-      case 'form.processState':
+      case 'feeds': {
+        console.log(value);
         break;
+      }
+      case 'posts': {
+        console.log(value);
+        break;
+      }
+
       default:
         break;
     }
   });
-
-  // *** utils ***
-  const buildFeed = (feedXmlDocument) => {
-    const feedId = _.uniqueId();
-    const feedTitle = feedXmlDocument.querySelector('title').textContent;
-    const feedDescription = feedXmlDocument.querySelector('description').textContent;
-    const feedLink = feedXmlDocument.querySelector('link').textContent;
-    const items = feedXmlDocument.querySelectorAll('item');
-    const feedPosts = Array.from(items).map((item) => {
-      const postId = _.uniqueId();
-      const postTitle = item.querySelector('title').textContent;
-      const postDescription = item.querySelector('description').textContent;
-      const postLink = item.querySelector('link').textContent;
-      return {
-        post: postId,
-        feedId,
-        title: postTitle,
-        description: postDescription,
-        link: postLink,
-      };
-    });
-
-    return {
-      id: feedId,
-      title: feedTitle,
-      description: feedDescription,
-      link: feedLink,
-      posts: feedPosts,
-    };
-  };
-
-  const parse = (data) => {
-    const parser = new DOMParser();
-    const feedXmlDocument = parser.parseFromString(data.data.contents, 'text/xml');
-    const feed = buildFeed(feedXmlDocument);
-
-    return feed;
-  };
 
   // *** CONTROLLERS ***
   form.addEventListener('input', (e) => {
@@ -137,9 +155,21 @@ export default () => {
     /* TODO: получить Фид */
     const urlWithAllOriginsProxy = `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(watchedState.form.value)}`;
     axios.get(urlWithAllOriginsProxy).then((response) => {
-      const feed = parse(response);
-      console.log(feed);
+      const feedData = parse(response);
+      console.log(feedData);
+      // добавить фид и посты в state
+      watchedState.feeds = [feedData.feed, ...watchedState.feeds];
+      // console.log(feedData.posts);
+      watchedState.posts = [...feedData.posts, ...watchedState.posts];
+
+      watchedState.form.valid = true;
+      watchedState.form.error = '';
+      watchedState.form.value = '';
+      submitButton.disabled = false;
+      watchedState.form.processState = 'finished';
     });
+
+    submitButton.disabled = true;
 
     // ********
 
