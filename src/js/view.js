ded4eba021a8630202c06d1bf08adf7b9db5a635
smilenb <alexander.is.dev@gmail.com>
2021-02-08 03:02:53 +0300

    Add(src/js/view.js) renderFeeds function

--- src/js/view.js
+++ src/js/view.js
@@ -14,12 +14,34 @@ export const renderFeedback = (error, element) => {
     element.classList.add('text-danger');
     return;
   }
-  console.log('success');
   element.classList.remove('text-danger');
-  element.textContent = 'Success но это надо уточнить!';
+  element.textContent = 'Rss has been loaded';
   element.classList.add('text-success');
 };
 
-export const renderFeeds = () => {};
+export const renderFeeds = (feeds, feedsBlock) => {
+  if (!feedsBlock.hasChildNodes()) {
+    const title = document.createElement('h2');
+    title.textContent = 'Feeds';
+    feedsBlock.append(title);
+    const ul = document.createElement('ul');
+    ul.classList.add('list-group', 'mb-5');
+    feedsBlock.append(ul);
+  }
+
+  const listGroup = feedsBlock.querySelector('.list-group');
+  listGroup.innerHTML = '';
+  feeds.forEach((feed) => {
+    const title = document.createElement('h3');
+    title.textContent = feed.title;
+    const description = document.createElement('p');
+    description.textContent = feed.description;
+
+    const item = document.createElement('li');
+    item.classList.add('list-group-item', 'border');
+    item.append(title, description);
+    listGroup.append(item);
+  });
+};
 
 export const renderPosts = () => {};
