9307bccfb26e927e1bbeaa455df6bcf8b3f04037
smlnx <alexander.is.dev@gmail.com>
2021-02-19 15:19:43 +0300

    Add(src/js/locales/) ru.js

---
+++ src/js/locales/ru.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    errors: {
+      notValidUrl: 'Ссылка должна быть валидным URL',
+      feedExists: 'RSS уже существует',
+      notValidRss: 'Этот источник не содержит валидный RSS',
+      noData: 'Запрос не выполнен. Нет данных',
+      notValidRssFormat: 'Неизвестный формат RSS-канала',
+    },
+    success: 'RSS успешно загружен',
+    preview: 'Просмотр',
+    feeds: 'Фиды',
+    posts: 'Посты',
+  },
+};
