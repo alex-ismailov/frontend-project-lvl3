fba95b84dce4e5530a3ac4b38b88df478cff9b43
smilenb <alexander.is.dev@gmail.com>
2021-02-27 16:47:54 +0300

    Refactor(src/parser.js) Hanging the id is not the responsibility of the parser

--- src/parser.js
+++ src/parser.js
@@ -1,38 +1,25 @@
-import _ from 'lodash';
-
-const buildFeed = (feedXmlDocument, feedUrl) => {
-  const feedId = _.uniqueId();
-  const feedTitle = feedXmlDocument.querySelector('title').textContent;
-  const feedDescription = feedXmlDocument.querySelector('description').textContent;
-  const items = feedXmlDocument.querySelectorAll('item');
-  const feedPosts = Array.from(items).map((item) => {
-    const postId = _.uniqueId();
-    const postTitle = item.querySelector('title').textContent;
-    const postDescription = item.querySelector('description').textContent;
-    const postLink = item.querySelector('link').textContent;
-    return {
-      id: postId,
-      feedId,
-      title: postTitle,
-      description: postDescription,
-      link: postLink,
-    };
-  });
-
-  return {
-    feedInfo: {
-      id: feedId,
-      title: feedTitle,
-      description: feedDescription,
-      link: feedUrl,
-    },
-    posts: feedPosts,
-  };
-};
-
 export default (data, feedUrl) => {
   const parser = new DOMParser();
   const feedXmlDocument = parser.parseFromString(data, 'application/xml');
 
-  return buildFeed(feedXmlDocument, feedUrl);
+  const xmlItems = feedXmlDocument.querySelectorAll('item');
+  const items = Array.from(xmlItems).map((item) => {
+    const title = item.querySelector('title').textContent;
+    const link = item.querySelector('link').textContent;
+    const description = item.querySelector('description').textContent;
+    return {
+      title,
+      link,
+      description,
+    };
+  });
+
+  const channel = {
+    title: feedXmlDocument.querySelector('title').textContent,
+    description: feedXmlDocument.querySelector('description').textContent,
+    link: feedUrl,
+    items,
+  };
+
+  return channel;
 };
